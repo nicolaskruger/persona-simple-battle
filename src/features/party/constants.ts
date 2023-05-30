493a1aeb16492d178ef5d04f9b245dd2b485daef
nkruger <nicolas.nkruger@gmail.com>
2023-05-30 15:09:17 -0300

    refactor: :recycle: transfer party members to constant folder

---
+++ src/features/party/constants.ts
@@ -0,0 +1,64 @@
+import { PartyMember } from "./partySlice";
+
+export const allPartyMembers: PartyMember[] = [
+  {
+    id: "0",
+    name: "Male Protagonist",
+    idPersona: "0",
+    img: "/party/male.webp",
+  },
+  {
+    id: "1",
+    name: "Female Protagonist",
+    idPersona: "1",
+    img: "/party/female.webp",
+  },
+  {
+    id: "2",
+    name: "Yukari Takeba",
+    idPersona: "2",
+    img: "/party/yukari.webp",
+  },
+  {
+    id: "3",
+    name: "Jumpei Iory",
+    idPersona: "3",
+    img: "/party/junpei.webp",
+  },
+  {
+    id: "4",
+    name: "Mitsuru Kirijo",
+    idPersona: "4",
+    img: "/party/mitsuru.webp",
+  },
+  {
+    id: "5",
+    name: "Akihiko Sanada",
+    idPersona: "5",
+    img: "/party/akihiko.webp",
+  },
+  {
+    id: "6",
+    name: "Aigis",
+    idPersona: "6",
+    img: "/party/aigis.webp",
+  },
+  {
+    id: "7",
+    name: "Koromaru",
+    idPersona: "7",
+    img: "/party/koromaru.webp",
+  },
+  {
+    id: "8",
+    name: "Ken Amada",
+    idPersona: "8",
+    img: "/party/ken.webp",
+  },
+  {
+    id: "9",
+    name: "Shinjiro",
+    idPersona: "9",
+    img: "/party/shinjiro.webp",
+  },
+];
--- src/features/party/partySlice.ts
+++ src/features/party/partySlice.ts
@@ -4,6 +4,7 @@ import {
   createSlice,
 } from "@reduxjs/toolkit";
 import { AppState } from "../../store";
+import { allPartyMembers } from "./constants";
 
 export type PartyMember = {
   name: string;
@@ -16,69 +17,6 @@ export type PartyState = {
   allMembers: EntityState<PartyMember>;
   selectedParty: EntityState<PartyMember>;
 };
-
-const allPartyMembers: PartyMember[] = [
-  {
-    id: "0",
-    name: "Male Protagonist",
-    idPersona: "0",
-    img: "/party/male.webp",
-  },
-  {
-    id: "1",
-    name: "Female Protagonist",
-    idPersona: "1",
-    img: "/party/female.webp",
-  },
-  {
-    id: "2",
-    name: "Yukari Takeba",
-    idPersona: "2",
-    img: "/party/yukari.webp",
-  },
-  {
-    id: "3",
-    name: "Jumpei Iory",
-    idPersona: "3",
-    img: "/party/junpei.webp",
-  },
-  {
-    id: "4",
-    name: "Mitsuru Kirijo",
-    idPersona: "4",
-    img: "/party/mitsuru.webp",
-  },
-  {
-    id: "5",
-    name: "Akihiko Sanada",
-    idPersona: "5",
-    img: "/party/akihiko.webp",
-  },
-  {
-    id: "6",
-    name: "Aigis",
-    idPersona: "6",
-    img: "/party/aigis.webp",
-  },
-  {
-    id: "7",
-    name: "Koromaru",
-    idPersona: "7",
-    img: "/party/koromaru.webp",
-  },
-  {
-    id: "8",
-    name: "Ken Amada",
-    idPersona: "8",
-    img: "/party/ken.webp",
-  },
-  {
-    id: "9",
-    name: "Shinjiro",
-    idPersona: "9",
-    img: "/party/shinjiro.webp",
-  },
-];
 
 const allMembersPartyAdapter = createEntityAdapter<PartyMember>({
   selectId: ({ id }) => id,
